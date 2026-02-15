88667b6c692cab68c6c022dd9b6c0ac17a639915
Patrick Warren <patrickwarren2692@gmail.com>
2026-02-16 08:38:51 +1100

    Full app rewrite with real-time partner sync

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDQOz-r2LIjk2Eyw19sy7yjzvhxafUFzKI",
-    authDomain: "family-clean-d428e.firebaseapp.com",
-    databaseURL: "https://family-clean-d428e-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "family-clean-d428e",
-    storageBucket: "family-clean-d428e.firebasestorage.app",
-    messagingSenderId: "1067832584063",
-    appId: "1:1067832584063:web:4f2c12e7374ccfeea75c4a"
+  apiKey: "AIzaSyDQOz-r2LIjk2Eyw19sy7yjzvhxafUFzKI",
+  authDomain: "family-clean-d428e.firebaseapp.com",
+  databaseURL: "https://family-clean-d428e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "family-clean-d428e",
+  storageBucket: "family-clean-d428e.firebasestorage.app",
+  messagingSenderId: "1067832584063",
+  appId: "1:1067832584063:web:4f2c12e7374ccfeea75c4a"
 });
 
 const messaging = firebase.messaging();
